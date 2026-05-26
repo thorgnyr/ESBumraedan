a7fc70669b321b5a791d8cfacaec672d913558e3
Þórgnýr Thoroddsen <thorgnyr@lanti.cc>
2026-05-26 18:13:30 +0000

    Update fetch_sentiment.py

--- scripts/fetch_sentiment.py
+++ scripts/fetch_sentiment.py
@@ -45,7 +45,7 @@ SOURCES = [
     },
 ]
 
-MAX_ARTICLES_PER_SOURCE = 6
+MAX_ARTICLES_PER_SOURCE = 50
 LOOKBACK_HOURS = 336
 OUTPUT_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "feed.json")
 
